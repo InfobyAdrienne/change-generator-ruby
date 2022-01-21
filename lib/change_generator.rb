fb028898f01586fd8925a6d541498d560883dc22
Adrienne Galloway <adriennegalloway@hotmail.com>
2022-01-21 15:27:42 +0000

    passing teh first test

---
+++ lib/change_generator.rb
@@ -0,0 +1,24 @@
+class Change_generator
+
+  def convert(amount)
+    if amount / 20 
+      return Array.new((amount / 20), "£20")
+    else 
+      return false
+    end
+  end
+
+  # private
+
+  # @change
+  # "£20"
+  # "£10"
+  # "£5"
+  # "£2"
+  # "£1"
+  # "50p"
+  # "20p"
+  # "2p"
+  # "1p"
+
+end 
