1fa16c4f625ce20899281226ac48a247c56e1cba
Adrienne Galloway <adriennegalloway@hotmail.com>
2022-01-23 18:57:02 +0000

    completed exercise - all tests passing

--- lib/change_generator.rb
+++ lib/change_generator.rb
@@ -1,25 +1,43 @@
-class Change_generator
+class ChangeGenerator
 
   def convert(amount)
-    if amount / 20 
-      return Array.new((amount / 20), "£20")
-    else 
-      return false
+    @conversion = [ ]
+      calculate(amount)
+    @conversion
+end
+
+  private
+
+  def calculate(amount)
+    amount = (amount * 100).ceil
+    units.each do |key, value|
+      if amount / key >= 1
+      i = 1
+        while i <= amount / key
+          @conversion << value
+          i += 1
+        end
+          amount -= key * (amount / key)
+      end 
     end
   end
 
-  # private
-
-  # @change
-  # "£20"
-  # "£10"
-  # "£5"
-  # "£2"
-  # "£1"
-  # "50p"
-  # "20p"
-  # "2p"
-  # "1p"
-  # making this change to commit 
-
+  def units
+    {
+      5000 => '£50',
+      2000 => '£20',
+      1000 => '£10',
+      500 => '£5',
+      100 => '£1',
+      50 => '50p',
+      20 => '20p',
+      10 => '10p',
+      5 => '5p',
+      2 => '2p',
+      1 => '1p'
+    }
+  end
 end 
+
+
+
--- spec/change_generator_spec.rb
+++ spec/change_generator_spec.rb
@@ -1,12 +1,21 @@
 require './lib/change_generator.rb'
 
-describe Change_generator do 
+describe ChangeGenerator do 
   subject (:change_generator) { described_class.new }
   
-  describe 'get change by dividing by two' do
-    it '£40 returns the change of £20 and £20' do 
-      expect(change_generator.convert(40)).to eq ["£20", "£20"]
+    it "£50 returns the change of £50" do
+      expect(change_generator.convert(50.00)).to eq(["£50"])
     end
-  end
+  
+    it 'retuns 1 coin in exact amount in pence' do
+      expect(subject.convert(0.05)).to eq ['5p']
+    end
 
+    it '£40 returns the change of £20 and £20' do 
+      expect(change_generator.convert(40.00)).to eq ["£20", "£20"]
+    end
+
+    it 'returns notes and coins' do
+      expect(subject.convert(7.42)).to eq ["£5", "£1", "£1", "20p", "20p", "2p"] 
+    end
 end 
